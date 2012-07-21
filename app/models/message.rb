ede75f534672b170b6633eaf5a2345396a6574c1
c <davidgroovey@gmail.com>
2012-07-21 13:52:23 -0500

    trabajo chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,14 +1,14 @@
 class Message < ActiveRecord::Base
 
-  Pusher.app_id = '14677'
-  Pusher.key = 'c0b97835ccce8d568a10'
-  Pusher.secret = '8181b30f36412d611cb2'
+  Pusher.app_id = '24390'
+  Pusher.key = '5da75bd64128b88d32b8'
+  Pusher.secret = 'f43743c6be287c61ad69'
 
   after_create :send_to_pusher
 
   def send_to_pusher
     logger.info "Hi?"
-    Pusher['demoChat'].trigger("message:create",self.to_json)  
+    Pusher['michat'].trigger("message:create",self.to_json)  
   end
 
 end
